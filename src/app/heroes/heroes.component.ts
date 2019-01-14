df85fe5efb3c0e3846abfd140471dd2152da4c85
Stephen Samonte Tan <30998408+stephensamonte@users.noreply.github.com>
2019-01-13 23:40:47 -0500

    Display List with Style
    Changes:
    - heroes.component.css added style
    - heroes.component.html added layout for list and detail item to display
    - heroes.component.ts added HEROES list property and onSelect(hero: Hero) mothod
    - Created mock-heroes.ts which is mock data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 // importing Hero type 
 import { Hero } from '../hero';
 
+// importing mockHeros
+import { HEROES } from '../mock-heroes';
+
 /** @Component is a decorator function that specifies the Angular metadata for the component. */ 
 @Component({
   // selector— the component's CSS element selector
@@ -12,18 +15,32 @@ import { Hero } from '../hero';
   // styleUrls— the location of the component's private CSS styles.
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
   // hero = 'Windstorm';
+
   // Object of type Hero named hero
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  // component property to expose MockHEROES array for binding data
+  heroes = HEROES;
+
+  // create selected hero property 
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // Function called when a hero in the list is selected
+  onSelect(hero: Hero): void {
+    // Set selected hero
+    this.selectedHero = hero;
   }
 
 }
