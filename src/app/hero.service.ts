627a49ea2769b76eda060c60fe44a0932c11cd5e
Stephen Samonte Tan <30998408+stephensamonte@users.noreply.github.com>
2019-01-18 06:09:19 -0500

    Show Message Service
    Changes:
    - Create the MessageService
    - injected services into the HeroService
    - Bind to the MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@angular/core';
 // you'll simulate getting data from the server with the RxJS of() function.
 import { Observable, of } from 'rxjs';
 
+// inject message into hero service
+// This is a typical "service-in-service" scenario: 
+// you inject the MessageService into the HeroService which 
+// is injected into the HeroesComponent.
+import { MessageService } from './message.service';
+
 // Import hero class
 import { Hero } from './hero';
 // Import mock hero list data 
@@ -13,13 +19,20 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  // constructor() { }
+  // a parameter that declares a private messageService property. 
+  // Angular will inject the singleton MessageService into that 
+  // property when it creates the HeroService.
+  constructor(private messageService: MessageService) { }
 
   // return the data 
   // getHeroes(): Hero[] {
   //   return HEROES;
   // }
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+
     // of(HEROES) returns an Observable<Hero[]> 
     // that emits a single value, the array of mock heroes.
     return of(HEROES);
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// To retrieve messages
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
+  
+  // Get userservice property as a parameter 
+  // Angular will inject the singleton MessageService into that 
+  // property when it creates the MessagesComponent.
+
+  // The messageService property must be public because you're about to 
+  // bind to it in the template.
+  constructor(public messageService: MessageService) {}
 
   ngOnInit() {
   }
