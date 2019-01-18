d98384f9bb7c1c2cc9dda2aacba8e6fd1f4f84c3
Stephen Samonte Tan <30998408+stephensamonte@users.noreply.github.com>
2019-01-18 05:43:13 -0500

    Show Message Message Component
    Changes:
    - Added Message Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 // NgModel lives here
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   // List of external modules the app needs 
   imports: [
